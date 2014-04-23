ea7a0f2f5166df3317ddbcc52b8dfe3042117d47
anirudhraok <anirudh.vy1091@gmail.com>
2014-04-23 04:28:47 -0500

    Update failover_script.pl
    updated flow entry and flow parameters grep commands.
    reconstruction of flow in line 31, using $flowvar collected by grep from @splitvalue.

--- failover_script.pl
+++ failover_script.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use warnings;
-use strict;
+
+system (" curl -d '{"switch": "00:00:de:de:06:b0:53:4a", "name":"flow1", "cookie":"0", "priority":"32768", "ingress-port":"eth1","active":"true", "actions":"output=2"}' http://192.168.164.131:8080/wm/staticflowentrypusher/json");
+#staticflow pusher usin controller1
 
 while(1) {
 my $t = time();
@@ -8,18 +10,31 @@ my $t = time();
 
 my $grepstatus = system('grep "is_connected: true" status.txt');
 
-system ("curl http://192.168.164.131:8080/wm/staticflowpusher/list/00:00:de:de:06:b0:53:4a : /json>flow.txt
-#list flows 
+`curl http://192.168.164.131:8080/wm/staticflowentrypusher/list/00:00:de:de:06:b0:53:4a/json > flow.txt`
+#list flows
+
+my @splitvalue = split(',', flow.txt);
+
+
+
+my $flowvar1 = grep(/^"cookie"/, @splitvalue);
+my $flowvar2 = grep(/^"priority"/, @splitvalue);
+my $flowvar3 = grep(/^"ingress-port"/,@splitvalue); 
+my $flowvar3 = grep(/^"actions"/,@splitvalue); 
+#obtaining flow parameters
 
 if ($grepstatus == 256)
 {
 print ("controller 1 has failed . connecting to controller 2..");
 system  ("ovs-vsctl set-controller br1 tcp:192.168.164.133:6633");
    # connecting to controller 2
+  system (" curl -d '{"switch": "00:00:de:de:06:b0:53:4a", "name":"flow1", "$flowvar1", "$flowvar2", "$flowvar3","active":"true", "$flowvar4"}' http://192.168.164.133:8080/wm/staticflowentrypusher/json");
+#staticflow pusher using controller2
 }
 else 
 {
    print("controller is stable");
+   
 }
 sleep(2);
 }
